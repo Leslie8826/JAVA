01b7ea4cd7085274b55955a875d868e46109f0ba
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-15 21:19:12 -0500

    Update currencyConverter.java

--- BASICS/Homework/currencyConverter.java
+++ BASICS/Homework/currencyConverter.java
@@ -1,1 +1,19 @@
+/* This program asks a user to enter an amount of money in euro currency. 
+   Then the program converts it into dollars. */
 
+import java.util.Scanner;
+
+public class currencyConverter {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(" Enter a number to convert: ");
+		double money = scanner.nextDouble();
+		double eurDol = 1.12 * money; // $1 = 1.12 euros on Jan 15th 2020
+		
+		System.out.println(money + " euro(s) = " + eurDol + " dollars");
+				
+	}
+
+}
