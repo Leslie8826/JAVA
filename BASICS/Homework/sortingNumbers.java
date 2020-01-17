a6c91fba4d049c19b5a0df14fae91cb74769602e
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-16 22:04:20 -0500

    Update sortingNumbers.java

--- BASICS/Homework/sortingNumbers.java
+++ BASICS/Homework/sortingNumbers.java
@@ -1,1 +1,45 @@
+/* This program sorts 3 numbers entered by the user in a descending order */
 
+import java.util.Scanner;
+
+public class sorting_3_numbers {
+	public static void main(String[] args) {
+		int a = 0, b = 0, c = 0;
+		
+		System.out.println("Enter a 1st number: ");
+		Scanner scan = new Scanner(System.in);
+		a = scan.nextInt();
+		
+		System.out.println("Enter a 2d number: ");
+		b = scan.nextInt();
+		
+		System.out.println("Enter a 3rd number: ");
+		c = scan.nextInt();
+	
+		
+// Sorted numbers in descending order
+	if (a < b) {
+		if (a < c) {
+			if (b < c) {
+				System.out.println("ordered numbers: " + c + " " + b + " " + a);
+			} else { // a<b, a<c, b>c
+				System.out.println("ordered numbers: " + b + " " + c + " " + a);
+			}
+		} else {//a<b, a > c
+			System.out.println("ordered numbers: " + b + " " + a + " " + c);
+		}
+	} else { // a > b
+		if (a < c) {
+		System.out.println("ordered numbers: " + c + " " + a + " " + b);
+	} else { // a>b, a>c
+		if (b>c) {
+			System.out.println("ordered numbers: " + a + " " + b + " " + c);
+		} else {
+			System.out.println("ordered numbers: " + a + " " + c + " " + b);
+		}
+	}
+	}
+	
+	
+}
+}
