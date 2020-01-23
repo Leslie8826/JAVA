a963b6b5a329656d5a99cb4fabd82c200ec733fb
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-23 13:03:21 -0500

    Create insertionSimulation_test3.java

---
+++ BASICS/Homework/insertionSimulation_test3.java
@@ -0,0 +1,65 @@
+
+
+import java.util.Scanner;
+
+public class array7_v3 {
+
+	public static void main(String[] args) {
+		int n = 4;
+		char[] ch = new char[n];
+		ch[0] = 'x';
+		ch[1] = 'r';
+		ch[2] = 'l';
+		ch[3] = 'b';
+		
+		System.out.println("The array is: ");
+		for (int i=0; i<ch.length; i++) {
+			System.out.print(" " + ch[i] + " ");
+		}
+		
+		System.out.print("\n");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("\n Enter the number of characters you want to insert: ");
+		int nb_char  = scan.nextInt();
+		
+		char[] ch2 = new char[n + nb_char];
+		
+		Scanner scan2 = new Scanner(System.in);
+		System.out.println("\n Indice you want to insert the characters?: ");
+		int indice_char = scan2.nextInt();
+		
+		int count = 0;
+		char charr = 'c';
+		
+		Scanner scan3 = new Scanner(System.in);
+		for (int p = 0; p < nb_char + n; p = p + 1) {
+			if(p < indice_char ) {
+				ch2[p] = ch[p];
+			//} else if(p >= indice_char && p <= nb_char) {
+			} else {
+				//if(p >= indice_char && p < nb_char + n) {
+				if(p >= indice_char && p < indice_char + nb_char) {
+					while (count < nb_char) {
+						System.out.println("Enter a character: ");
+					    charr = scan3.next().charAt(0);
+					    ch2[p] = charr;
+					    count = count + 1;
+					}
+				}
+				if (p >= indice_char+nb_char && p <= nb_char+n) {
+					ch2[p] = ch[p-nb_char];
+					}
+			}
+		}
+		
+		System.out.println("\n The new array is: ");
+		for (int l=0; l < n+nb_char; l++) {
+			System.out.print(" " + ch2[l] + " ");
+		}
+		
+	}
+
+	
+
+}
