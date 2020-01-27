5e6fb7a0188396b3c6a03aa216379257a987633f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-26 21:08:17 -0500

    Update arrayList_grades.java

--- BASICS/Homework/arrayList_grades.java
+++ BASICS/Homework/arrayList_grades.java
@@ -1,1 +1,64 @@
+// This program creates an ArrayList of grades. Then it diplays the grades stored (using a method).
+// It computes the average of the grades (using a method) and adds grades (using a method)
+
+import java.util.ArrayList;
+
+public class arraylist {
+
+	// method that computes the average of grades
+	static void addGrade(ArrayList<Double> gradeList, Double grade) {
+		gradeList.add(grade);
+	}
+	
+	// method that displays the the grades stored in the ArrayList
+	static void displayGrades(ArrayList<Double> gradeList) {
+		for(int i=0; i<gradeList.size();i=i+1) {
+			System.out.print(gradeList.get(i) + "\t");
+		}
+	}
+	
+	// method that computes the average of grades
+	static double average(ArrayList<Double> gradeList) {
+		double average = 0.0;
+		double sum = 0.0;
+		
+		for(int i=0; i<gradeList.size();i=i+1) {
+			sum = sum + gradeList.get(i);
+		}
+		
+		average = sum/gradeList.size();
+		
+		return average;
+	};
+	
+	public static void main(String[] args) {
+		ArrayList gradeList = new ArrayList<Double>();
+		
+		// add grades to the list of grades
+		addGrade(gradeList, 92.5);
+		addGrade(gradeList, 94.75);
+		addGrade(gradeList, 90.0);
+		
+		/*gradeList.add(92.75);
+		gradeList.add(95.65);
+		gradeList.add(97.0);*/
+		
+		// displays the list of grades
+		System.out.println("List of grades: ");
+		displayGrades(gradeList);
+		
+		// computes the average of grades
+		System.out.println("\n");
+		System.out.println(" Average of grades: ");
+		System.out.println(average(gradeList));
+		
+		// adds a new grade
+		System.out.println("\n Let's add a new grade. ");
+		addGrade(gradeList, 96.5);
+		
+		// displays the new list of grades
+		System.out.println("\n New list of grades: ");
+		displayGrades(gradeList);
+	}
+}
 
