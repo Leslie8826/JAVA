957d1e38d5cf666ea4716c48623e873512a451ee
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-28 22:38:02 -0500

    Update MCQ.java

--- BASICS/Project/MCQ.java
+++ BASICS/Project/MCQ.java
@@ -1,1 +1,12 @@
 
+import java.util.Scanner;
+
+public class Final_Project_EN
+{
+	public static void main(String[] args) 
+	{
+		// Asks the professor to type the numnber of questions of the Multiple Choice Questions 
+		int number_questions=0, number_answers=0;
+		System.out.println("Type the number of questions: ");
+		Scanner scan_numb_questions = new Scanner(System.in);
+		number_questions = scan_numb_questions.nextInt();
