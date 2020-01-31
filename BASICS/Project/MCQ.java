3bb6f998750fd160f8b51bcbdfe379715fe6d63e
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-01-31 10:51:33 -0500

    Update MCQ.java

--- BASICS/Project/MCQ.java
+++ BASICS/Project/MCQ.java
@@ -2,12 +2,19 @@
 // First, the program asks the professor to enter the details about
 // the mcq: number of questions, number of answers per questions (assuming 
 // there will be the same number of answers for each question).
+//
 // Then, the program asks the professor to type each question along with
 // the choice of answers and the correct answer to that question.
 // Finally, the program students take the questionnaire and enter their answers
 // to each question.
+//
 // A final result will be given to students in how many questions out of the total 
-// number of questions
+// number of questions. If the student succeed in answering at least half of the
+// questions, he passes the test. Otherwise the student fails. 
+// A message of success or failure has to be displayed at the end of the MCQ.
+//
+// The student can retake the test if they want to. At the end of the program, the student
+// will be asked to retake the MCQ.
 
 import java.util.Scanner;
 
