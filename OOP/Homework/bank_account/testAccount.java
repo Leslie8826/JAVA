6b85fd2e558f01fa7a5e90770bfab359669e369b
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-02 22:46:54 -0500

    Update testAccount.java

--- OOP/Homework/bank_account/testAccount.java
+++ OOP/Homework/bank_account/testAccount.java
@@ -1,1 +1,27 @@
+// This program create an account object and tests its methods and instance variables
+
+public class runAccount {
+	public static void main(String[] args) {
+		account acc1 = new account();
+		System.out.println("Balance on account: " + acc1.balance);
+		
+		acc1.account_number = 71253695;
+		
+		System.out.println("\n Balance for account number " + acc1.account_number  + ": ");
+		acc1.display_balance();
+		
+		int deposit1 = acc1.deposit(2500);
+		System.out.println("\n Balance for account number " + acc1.account_number + " after deposit of $" + deposit1 + ": ");
+		acc1.display_balance();
+		
+		int deposit2 = (acc1.deposit(2500) - deposit1 );
+		System.out.println("\n Balance for account number " + acc1.account_number + " after deposit of $" + deposit2 + ": ");
+		
+		int deposit3 = acc1.deposit(7500) - deposit2;
+		System.out.println("\n Balance for account number " + acc1.account_number + " after deposit of $" + deposit3 + ": ");
+		
+		
+	}
+
+}
 
