5f88f73391764791492c157e597601057512b785
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-02 22:43:58 -0500

    Update account.java

--- OOP/Homework/bank_account/account.java
+++ OOP/Homework/bank_account/account.java
@@ -1,1 +1,22 @@
+// This class represents the bank account object with the instance
+// variables: balance (of the account), account_holder (name of the bank account holder) 
+// as well as the account_number. 
+// This class also has 2 methods to display the balance of the account and 
+// to add money in the account. The latter will return the current balance of the account.
+// 
+
+public class account {
+	int balance;
+	String account_holder; 
+	int account_number;
+	
+	void display_balance(){
+		System.out.println("balance = "+ this.balance); 
+	}
+	
+	int deposit(int amount){
+		this.balance = this.balance +  amount;
+		return this.balance; 
+	}
+}
 
