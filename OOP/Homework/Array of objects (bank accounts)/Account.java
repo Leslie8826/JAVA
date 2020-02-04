905445b4d2c0985df8aefac98154c6c747e71726
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-04 12:57:52 -0500

    Update Account.java

--- OOP/Homework/Array of objects (bank accounts)/Account.java
+++ OOP/Homework/Array of objects (bank accounts)/Account.java
@@ -1,1 +1,23 @@
 
+public class Account {
+	double balance = 0;
+	
+	double deposit(double amount) {
+		 balance = balance + amount;
+		 return balance;
+	}
+	
+	double withdraw(int amount) {
+		return balance = balance - amount;
+	}
+	
+	void transfer(int amount, Account destination) {
+		this.withdraw(amount);
+		destination.deposit(amount);
+	}
+	
+	void display() {
+		System.out.println("balance: " + balance);
+	}
+}
+
