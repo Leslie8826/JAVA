e881a1c81ae0da5a5dc90acfb255840bd5c505a0
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-04 10:37:17 -0500

    Update Account.java

--- OOP/Homework/bank_operations/Account.java
+++ OOP/Homework/bank_operations/Account.java
@@ -1,1 +1,22 @@
 
+public class Account {
+	int balance = 0;
+	
+	void deposit(int amount) {
+		balance = balance + amount;
+	}
+	
+	void withdraw(int amount) {
+		balance = balance - amount;
+	}
+	
+	void transfer(int amount, Account destination) {
+		this.withdraw(amount);
+		destination.deposit(amount);
+	}
+	
+	void display() {
+		System.out.println("balance: " + balance);
+	}
+}
+
