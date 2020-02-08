a480f2fcc0c439a124fe9fbd9fc74479314564bf
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-08 16:16:32 -0500

    Update testLibrary.java

--- OOP/Homework/Library/testLibrary.java
+++ OOP/Homework/Library/testLibrary.java
@@ -1,1 +1,39 @@
+public class tesLibrary {
+	public static void main(String[] args) {
+		book b1 = new book();
+		book b2 = new book();
+		library l = new library();
+		
+		
+		// Book 1
+		b1.setTitle("Introduction to Robotics");
+		b1.setAuthor("Colin D. Simpson");
+		b1.setEditor("Ray Santers Edition");
+		
+		/*System.out.println("First book: ");
+		System.out.println("Title --> " + b1.getTitle());
+		System.out.println("Author --> " + b1.getAuthor());
+		System.out.println("Editor --> " + b1.getEditor());*/
+		
+		// Book 2
+		b2.setTitle("Handbook of Robotics");
+		b2.setAuthor("Bruno Siciliano");
+		b2.setEditor("Springer Edition");
+		
+		/*System.out.println("\n Second book: ");
+		System.out.println("Title --> " + b2.getTitle());
+		System.out.println("Author --> " + b2.getAuthor());
+		System.out.println("Editor --> " + b2.getEditor());*/
+		
+		
+		// Add books b1 and b2 in the library
+		l.addBook(b1);
+		l.addBook(b2);
+		
+		// Displays the books b1 and b2 in library
+		System.out.println("\n Books in library:");
+		l.displayLibrary();
+		
+	}
+}
 
