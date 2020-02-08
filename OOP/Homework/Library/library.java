39d7fc02744d149cdf71131847fca770246f4d59
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-08 16:16:01 -0500

    Update library.java

--- OOP/Homework/Library/library.java
+++ OOP/Homework/Library/library.java
@@ -1,1 +1,19 @@
+import java.util.ArrayList;
 
+public class library {
+	ArrayList<book> library = new ArrayList<book>();
+	
+	// Method to add books in library
+	ArrayList<book> addBook(book book) {
+		library.add(book);
+		return library;
+	}
+	
+	// Method to display all books in library
+	void displayLibrary(){
+		for (int i=0; i<library.size(); i++) {
+			System.out.println(library.get(i).getTitle());
+		}
+	}
+	
+}
