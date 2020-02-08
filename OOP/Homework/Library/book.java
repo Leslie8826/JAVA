7b797e32cbb9e49ff971724379dd0dabb42ef8da
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-08 16:15:33 -0500

    Update book.java

--- OOP/Homework/Library/book.java
+++ OOP/Homework/Library/book.java
@@ -1,1 +1,30 @@
+public class book {
+	String Title;
+	String Author;
+	String Editor;
+	
+	public void setTitle(String s){
+		Title = s;
+	}
+	
+	public void setAuthor(String s){
+		Author = s;
+	}
+	
+	public void setEditor(String s){
+		Editor = s;
+	}
+	
+	public String getTitle() {
+		return Title;
+	}
+	
+	public String getAuthor() {
+		return Author;
+	}
+	
+	public String getEditor() {
+		return Editor;
+	}
+}
 
