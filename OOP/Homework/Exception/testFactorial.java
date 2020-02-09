ed9369e5bf10a56c670b9c52ddfa6ddda3c4f669
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-08 19:10:31 -0500

    Update testFactorial.java

--- OOP/Homework/Exception/testFactorial.java
+++ OOP/Homework/Exception/testFactorial.java
@@ -1,1 +1,15 @@
+public class testFactorial {
+  
+	public static void main(String[] args) {
+    
+		System.out.println("Factorial(5) = " + Factorial.factorial(5));
+		
+		System.out.println("\n Factorial(10) = " + Factorial.factorial(10));
+		
+		System.out.println("\n Factorial(3) = " + Factorial.factorial(3));
+		
+		System.out.print("\n Factorial(-1) = " + Factorial.factorial(-1));
+	}
+
+}
 
