4a1a40d24dcea9150abebe24d6db43cee3a1ae0e
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-08 19:07:55 -0500

    Update Factorial.java

--- OOP/Homework/Exception/Factorial.java
+++ OOP/Homework/Exception/Factorial.java
@@ -1,1 +1,13 @@
-
+static int factorial(int n){
+		int res = 1; 
+		
+		if (n<0){
+			throw new Not_defined(); 
+		}
+		
+		for(int i = 1; i <= n; i++){ 
+			res = res *i;
+		}
+		
+		return res; 
+	}
