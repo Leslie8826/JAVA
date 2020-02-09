0f85260b0e367624d4731b637be29b6ff8fed1b8
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-09 14:57:20 -0500

    Update MainClass.java

--- OOP/Homework/Interface/MainClass.java
+++ OOP/Homework/Interface/MainClass.java
@@ -1,1 +1,19 @@
+public class MainClass {
+	public static void main(String[] args) {
+		AnimalScream[] screams = new AnimalScream[6];
+		
+		screams[0] = new Dog();
+		screams[1] = new Cat();
+		screams[2] = new Cat();
+		screams[3] = new Rabbit();
+		screams[4] = new Dog();
+		screams[5] = new Cat();
+		
+		for (int i=0; i<6; i=i+1) {
+			System.out.print("Animal " + i + " says: ");
+			screams[i].scream();
+		}
+		
+	}
+}
 
