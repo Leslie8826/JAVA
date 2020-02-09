9a6293eef7a159125fa0bfb720a13b9293c1e6b4
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-09 11:18:31 -0500

    Update Stop.java

--- OOP/Homework/try Catch/Stop.java
+++ OOP/Homework/try Catch/Stop.java
@@ -1,1 +1,29 @@
 
+public class Stop {
+	
+	static int l(int x) {
+		
+		if (x < 0) {
+			throw new Stop2();
+		}
+		
+		return x; 
+	}
+	
+	public static void main(String[] args) {
+		int y = 0;
+		
+		try {
+			System.out.println("Hello 1"); 
+			y = l(-2); 
+			
+			System.out.println("Hello 2");
+			} catch (Stop2 e) { 
+				System.out.println("Hello 3");
+			}
+		
+		System.out.println("\n y = " + y); 
+		
+	}
+}
+
