df9904f452218e94e2f3989c020c47e65fad8267
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-10 11:29:22 -0500

    Update BankAccountWithOverdraft.java

--- OOP/Homework/inheritance/Bank Account/BankAccountWithOverdraft.java
+++ OOP/Homework/inheritance/Bank Account/BankAccountWithOverdraft.java
@@ -1,1 +1,22 @@
+public class BankAccountWithOverdraft extends BankAccount{
+	double maxOverdraft ;
+	
+	public void setMaxOverdraft(double amount){ 
+		this.maxOverdraft = amount ;
+	}
+	
+	public BankAccountWithOverdraft (String holder, char[] num, double amount, double maxOverdraft){ 
+		super(holder, num, amount);
+		this.maxOverdraft = maxOverdraft; 
+	}
+	
+	public void withdraw(double amount){
+		if (this.balance - amount < (- maxOverdraft)){
+			throw new InsufficientBalanceError(); 
+			} else {
+				balance = balance - amount; 
+			}
+		}
+}
+	
 
