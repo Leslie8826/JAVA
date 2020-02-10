d9a61aeec11a71eff223b1602434f0077b3b6afc
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-10 12:52:58 -0500

    Update Employee.java

--- OOP/Homework/inheritance/Employees/Employee.java
+++ OOP/Homework/inheritance/Employees/Employee.java
@@ -1,1 +1,33 @@
+public class Employee {
+	String employeeName;
+	int id;
+	double salary_index;
+	static double salary = 7800;
 
+	// Constructor to set the name, the id and the salary index of an employee
+	public Employee(String name, int id, double index) {
+		this.employeeName = name;
+		this.id = id;
+		this.salary_index = index;
+	}
+	
+	// Method to get the name of an employee
+	public String getName() {
+		return this.employeeName;
+	}
+	
+	// Method to get the id of an employee
+	public int getId() {
+		return this.id;
+	}
+	
+	// Method to get the salary index of an employee
+	public double getSalIndex() {
+		return this.salary_index;
+	}
+	
+	// Method to get the yearly salary of an employee
+	double getSalary() {
+		return this.salary_index*salary;
+	}
+}
