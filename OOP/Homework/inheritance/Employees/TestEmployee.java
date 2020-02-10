11fe7de66b7bb51e01277ef0101fabd9c43f375a
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-10 12:53:22 -0500

    Update TestEmployee.java

--- OOP/Homework/inheritance/Employees/TestEmployee.java
+++ OOP/Homework/inheritance/Employees/TestEmployee.java
@@ -1,1 +1,15 @@
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		Employee employee1 = new Employee("Karl", 1, 7.22);
+		
+		System.out.println("First employee information: ");
+		System.out.println("Employee name: " + employee1.getName());
+		System.out.println("Employee id: " + employee1.getId());
+		System.out.println("Employee salary index: " + employee1.getSalIndex());
+		System.out.println("Employee yearly salary: " + employee1.getSalary());
+		
+	}
+
+}
 
