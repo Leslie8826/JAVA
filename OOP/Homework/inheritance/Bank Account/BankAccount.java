c3877ea6e89348fe40981408e14d299a9482d46c
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-10 11:28:45 -0500

    Update BankAccount.java

--- OOP/Homework/inheritance/Bank Account/BankAccount.java
+++ OOP/Homework/inheritance/Bank Account/BankAccount.java
@@ -1,1 +1,37 @@
+public class BankAccount {
+	String nameAccountHolder ; 
+	char[] number ;
+	double balance ;
+	
+	public BankAccount(){	
+	}
+	
+	public BankAccount(String holder, char[] num, double amount){ 
+		this.nameAccountHolder = holder ;
+	    this.number = num ;
+	    this.balance = amount ;
+	}
+	
+	public double getCurrentBalance (){ 
+		return this.balance ;
+	}
+	
+	public void deposit(double amount){ 
+		this.balance = balance + amount ;
+	}
+	
+	public void withdraw(double amount){
+		System.out.println("Withdrwal on bank account"); 
+		if (this.balance < amount){
+			throw new InsufficientBalanceError() ; 
+		} else {
+		    balance = balance - amount ; 
+		    }
+		}
+	
+	public void transfer(BankAccount c, double amount){
+		c.withdraw(amount);
+	    this.deposit(amount);
+	}
+}
 
