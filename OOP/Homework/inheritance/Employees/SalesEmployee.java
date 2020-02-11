536135f4a44f3a579b8afab8aee927cb8b61e1e4
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-11 11:52:59 -0500

    Create SalesEmployee.java

---
+++ OOP/Homework/inheritance/Employees/SalesEmployee.java
@@ -0,0 +1,26 @@
+public class SalesEmployees extends Employee{
+	double base = Employee.value;
+	double salesResults;
+	double salary_index;
+	
+	public SalesEmployees(String name, int id, double index) {
+		super(name, id, index);
+		this.salary_index = index;
+	}
+	
+	public void setSalesResults(double sales) {
+		salesResults = sales;
+	}
+	
+	public double getSalesResults() {
+		return this.salesResults;
+	}
+	
+	double getSalary2() {
+		double bonus = this.salesResults * 0.25;
+		double salary = (base + bonus) * this.salary_index;
+		return salary;
+	}
+	
+	
+}
