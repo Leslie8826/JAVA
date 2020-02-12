c54a0be01f22140459f74928fcd277e12bf746ec
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-12 11:55:51 -0500

    Update Person.java

--- OOP/Homework/inheritance/Person2/Person.java
+++ OOP/Homework/inheritance/Person2/Person.java
@@ -1,1 +1,16 @@
+public class Person {
+	Date birthdate; 
+	String name;
+	
+	public Person(String name, Date birth) { 
+		this. name = name;
+		this.birthdate = birth;
+	}
+	
+	public void write() { 
+		System.out.print(name + " was born on "); 
+		birthdate.write(); 
+		System.out.print(" (date format ---> d/m/y)");
+	}
+}
 
