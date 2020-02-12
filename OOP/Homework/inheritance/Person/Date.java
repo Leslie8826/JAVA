1009388a2b1acc02b0b6460fd476ec0580c8d82e
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-12 11:13:52 -0500

    Update Date.java

--- OOP/Homework/inheritance/Person/Date.java
+++ OOP/Homework/inheritance/Person/Date.java
@@ -1,1 +1,19 @@
+public class Date {
+	int day;
+	int month;
+	int year;
+	static int CURRENT_YEAR = 2020;
+	
+  // Constructor to set day, month and year
+	public Date (int d, int m, int y){ 
+		this.day = d;
+		this.month = m;
+		this.year = y; 
+	}
+	
+	public void write() { 
+		System.out.print(this.day + "/" + this.month + "/" + this.year);
+	   } 
+	
+}
 
