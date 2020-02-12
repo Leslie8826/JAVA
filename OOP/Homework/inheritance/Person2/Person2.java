6299e481d4c58f59fe07a2c644a204f80e4430dd
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-12 11:56:14 -0500

    Update Person2.java

--- OOP/Homework/inheritance/Person2/Person2.java
+++ OOP/Homework/inheritance/Person2/Person2.java
@@ -1,1 +1,14 @@
+public class Person2 extends Person {
+	String birthdate_place;
+	
+	public Person2(String name, Date birth, String place) {
+		super(name, birth);
+		this.birthdate_place = place;
+	}
+	
+	public void write() { 
+		super.write();
+		System.out.print(" in " + this.birthdate_place);
+	}	
+}
 
