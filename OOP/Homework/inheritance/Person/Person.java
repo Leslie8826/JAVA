234380982d4e4d32055d895d61a67f90aa326c59
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-12 11:18:33 -0500

    Update Person.java

--- OOP/Homework/inheritance/Person/Person.java
+++ OOP/Homework/inheritance/Person/Person.java
@@ -1,1 +1,16 @@
+public class Person {
+	Date birthdate; 
+	String name;
+	
+	public Person(String name, Date birth) { 
+		this. name = name;
+		this.birthdate = birth;
+	}
+	
+	public void write() { 
+		System.out.print(name + "'s DOB d/m/y = ("); 
+		birthdate.write(); 
+		System.out.print(")");
+	}
+}
 
