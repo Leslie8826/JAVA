46e0ddc53edcd132db3a4819426a2dd514b0139d
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-12 11:56:38 -0500

    Update TestPerson.java

--- OOP/Homework/inheritance/Person2/TestPerson.java
+++ OOP/Homework/inheritance/Person2/TestPerson.java
@@ -1,1 +1,13 @@
+public class TestPerson {
+
+	public static void main(String[] args) {
+		Person Jane = new Person("Jane", new Date (12, 4, 2006));
+		Jane.write(); // write() method from the Person class
+		
+		System.out.println("\n");
+		Person2 Karl = new Person2("Karl", new Date(1, 1, 1998), "New York");
+		Karl.write(); // write() method from the Person2 class
+	}
+
+}
 
