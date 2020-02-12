7aa658fa2b1c1a152f320feaf7f2c5ff1b6fb503
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-12 11:55:01 -0500

    Update Date.java

--- OOP/Homework/inheritance/Person2/Date.java
+++ OOP/Homework/inheritance/Person2/Date.java
@@ -1,1 +1,17 @@
+public class Date {
+	int day;
+	int month;
+	int year;
+	static int CURRENT_YEAR = 2020;
+	
+	public Date (int d, int m, int y){ 
+		this.day = d;
+		this.month = m;
+		this.year = y; 
+	}
+	
+	public void write() { 
+		System.out.print(this.day + "/" + this.month + "/" + this.year);
+	   } 
+}
 
