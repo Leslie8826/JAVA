9da9b4175e1c5aca2d0bd1f74ec48f4d4e82b423
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-15 13:00:42 -0500

    Update Palindrome.java

--- OOP/Homework/Palindrome/Palindrome.java
+++ OOP/Homework/Palindrome/Palindrome.java
@@ -2,11 +2,15 @@ public class Palindrome {
 	void palindrome(String str) {
 		int count = 0;
 		for(int p = 0; p < str.length(); p++) {
+			// compares the letters read from left to right to the letters read from right to left
 			if(str.charAt(p) == str.charAt(str.length()-1 - p)) {
+				// counts the number of letters that are similar
 				count = count + 1;
 			}
 		}
 		
+		/* if the number of letters read from left to right are similar to the letters read from right to left
+		we have a palindrome */
 		if (count == str.length()) {
 			System.out.println(str + " is a palindrome.");
 		} else {
