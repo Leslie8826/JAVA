6653526d75fceede92607af07b287733978e9a6a
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-15 12:48:03 -0500

    Update TestPalindrome.java

--- OOP/Homework/Palindrome/TestPalindrome.java
+++ OOP/Homework/Palindrome/TestPalindrome.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class TestPalindrome {
+	public static void main(String[] args) {
+		String s;
+		Palindrome pal = new Palindrome();
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Type a word: ");
+		s = scan.nextLine();
+		
+		pal.palindrome(s);
+	}
+}
 
