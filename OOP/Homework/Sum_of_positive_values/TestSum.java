84e93a607745871017141b7828adcd1015ca295e
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-15 13:25:40 -0500

    Update TestSum.java

--- OOP/Homework/Sum_of_positive_values/TestSum.java
+++ OOP/Homework/Sum_of_positive_values/TestSum.java
@@ -1,1 +1,27 @@
+/*This program computes the sum of positive values in an array
+ * We create a class Sum in which we create a sum function
+ * */
+
+import java.util.Scanner;
+
+public class TestSum {
+	public static void main(String[] args) {
+		int n = 200;
+		int[] nb = new int[n];
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the length of the array: ");
+		n = scan.nextInt();
+		System.out.print("\n");
+		
+		for(int i=0; i<n; i++) {
+			System.out.println("Enter a value: ");
+			nb[i] = scan.nextInt();
+		}                  
+		
+		Sum sum = new Sum();
+		System.out.println("\n");
+		System.out.println("Sum of positive values: " + sum.sum(nb));
+	}
+}
 
