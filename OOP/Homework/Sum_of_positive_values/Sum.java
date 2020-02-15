4216eabc30bd8b27bbde143a04cbb30e6ada3c46
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-15 13:26:25 -0500

    Update Sum.java

--- OOP/Homework/Sum_of_positive_values/Sum.java
+++ OOP/Homework/Sum_of_positive_values/Sum.java
@@ -1,1 +1,13 @@
+public class Sum {
+	int sum(int[] array) {
+		int sum =0;
+		
+		for(int l = 0; l < array.length; l++) {
+			if (array[l] > 0) {
+				sum = sum + array[l];
+			}
+		}
+		return sum;
+	}
+}
 
