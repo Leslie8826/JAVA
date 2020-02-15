bb926f7648d38a8c01dd65fefbbc404509b63f56
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-15 12:47:32 -0500

    Update Palindrome.java

--- OOP/Homework/Palindrome/Palindrome.java
+++ OOP/Homework/Palindrome/Palindrome.java
@@ -1,1 +1,17 @@
+public class Palindrome {
+	void palindrome(String str) {
+		int count = 0;
+		for(int p = 0; p < str.length(); p++) {
+			if(str.charAt(p) == str.charAt(str.length()-1 - p)) {
+				count = count + 1;
+			}
+		}
+		
+		if (count == str.length()) {
+			System.out.println(str + " is a palindrome.");
+		} else {
+			System.out.println(str + " is not a palindrome.");
+		}
+	}
+}
 
