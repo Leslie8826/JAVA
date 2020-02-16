c475cd08b7a53ccdb608e7e88ddc86023a8766cf
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-16 11:55:32 -0500

    Update TestPalindrome.java

--- OOP/Homework/Int Palindrome/TestPalindrome.java
+++ OOP/Homework/Int Palindrome/TestPalindrome.java
@@ -1,1 +1,9 @@
+public class TestPalindrome {
+	public static void main(String[] args) {
+		int nb = 12111;
+		palindrome pal = new palindrome(nb);
+
+		System.out.println("Is " + nb + " a palindrome? \n" + pal.isPalindrome(nb));
+  }
+}
 
