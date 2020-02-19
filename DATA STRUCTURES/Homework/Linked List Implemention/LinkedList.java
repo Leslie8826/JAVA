10aa18866aa72ab98baee9457aac232c951c90fa
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-19 10:13:14 -0500

    Update LinkedList.java

--- DATA STRUCTURES/Homework/Linked List Implemention/LinkedList.java
+++ DATA STRUCTURES/Homework/Linked List Implemention/LinkedList.java
@@ -1,1 +1,60 @@
 
+public class TheLinkedList {
+	private Node head;
+
+	public TheLinkedList() {
+		head = null;
+	}
+	
+	// Creates a node with a value and adds it at 
+	// the first position of the linked list
+	public void addFirst(String value) {
+		Node node = new Node(value);
+		node.next = head;
+		head = node;
+	}
+	
+	// Creates a node with a value and adds it 
+	// at the last position of the linked list
+	public void addLast(String value) {
+		Node node = new Node(value);
+		Node i = head;
+
+		while (i.next != null) {
+			i = i.next;
+		}
+		
+		i.next = node;
+	}
+
+	
+	// Creates a node with a value and adds it after 
+	// another node with a certain value
+	public void addAfter(String afterValue, String newValue) {
+		Node node = new Node(newValue);
+		Node i = head;
+
+		while (i.value != afterValue) {
+			i = i.next;
+		}
+		
+		node.next = i.next;
+		i.next = node;
+	}
+	
+	// Displays the Linked List
+	public void display() {
+		Node i = head;
+		
+		System.out.println("The linked list:");
+
+		while (i.next != null) {
+			System.out.print(i.value + " ---> ");
+			i = i.next;
+		}
+		
+		System.out.println(i.value);
+	}
+
+}
+
