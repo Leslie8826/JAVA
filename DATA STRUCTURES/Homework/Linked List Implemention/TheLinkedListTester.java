fef3ce5038e1c6d02783d4b10ccefa7cd04619dc
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-19 10:14:07 -0500

    Update TheLinkedListTester.java

--- DATA STRUCTURES/Homework/Linked List Implemention/TheLinkedListTester.java
+++ DATA STRUCTURES/Homework/Linked List Implemention/TheLinkedListTester.java
@@ -1,1 +1,34 @@
+public class TheLinkedListTester {
+
+	public static void main(String[] args) {
+		TheLinkedList linkedList = new TheLinkedList();
+		
+		linkedList.addFirst("Paris");
+		
+		System.out.println("Dislay 1:");
+		linkedList.display();
+		
+		linkedList.addLast("Montreal");
+		
+		System.out.println("\n Dislay 2:");
+		linkedList.display(); 
+		
+		
+		linkedList.addLast("Beijing");
+		linkedList.addFirst("New York");
+		
+		System.out.println("\n Dislay 3:");
+		linkedList.display(); 
+		
+		linkedList.addAfter("Paris", "Tokyo");
+		
+		System.out.println("\n Dislay 4:");
+		linkedList.display();
+		
+		linkedList.addAfter("Tokyo", "Toronto");
+		System.out.println("\n Dislay 5:");
+		linkedList.display();
+	}
+
+}
 
