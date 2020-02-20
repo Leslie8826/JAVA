57fab713974eeabf93689d27d8b3f5eb97fc0ded
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-20 10:39:10 -0500

    Update TheLinkedList.java

--- DATA STRUCTURES/Homework/Store With A Linked List/TheLinkedList.java
+++ DATA STRUCTURES/Homework/Store With A Linked List/TheLinkedList.java
@@ -1,1 +1,76 @@
 
+public class TheLinkedList {
+	public Node head;
+
+	public TheLinkedList() {
+		head = null;
+	}
+	
+	// Creates a node with a value and adds it at 
+	// the first position of the linked list
+	public void addFirst(String value) {
+		Node node = new Node(value);
+		node.next = head;
+		head = node;
+	}
+	
+	// Creates a node with a value and adds it 
+	// at the last position of the linked list
+	public void addLast(String value) {
+		Node node = new Node(value);
+		Node i = head;
+
+		while (i.next != null) {
+			i = i.next;
+		}
+		
+		i.next = node;
+	}
+
+	
+	// Creates a node with a value and adds it after 
+	// another node with a certain value
+	public void addAfter(String afterValue, String newValue) {
+		Node node = new Node(newValue);
+		Node i = head;
+
+		while (i.value != afterValue) {
+			i = i.next;
+		}
+		
+		node.next = i.next;
+		i.next = node;
+	}
+	
+	// Deletes a node when it is found. 
+	// Works only for all nodes that are different from the head
+	public void delete(String value) {
+		// Stores head node and defines previous node.
+		Node node = head;
+		Node previousNode = null;
+		
+		// Searches for the string to be deleted
+		while(node != null && node.value != value) {
+			previousNode = node;
+			node = node.next;
+		}
+    
+		if(node != null) {
+			previousNode.next = node.next;
+		}
+	}
+	
+	// Displays the Linked List
+	public void display() {
+		Node i = head;
+
+		while (i.next != null) {
+			System.out.print(i.value + " ---> ");
+			i = i.next;
+		}
+		
+		System.out.println(i.value);
+	}
+
+}
+
