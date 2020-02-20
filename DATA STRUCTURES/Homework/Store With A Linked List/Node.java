54a7005558b4f8d58cd399a709100ace5bf412e8
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-20 10:37:56 -0500

    Update Node.java

--- DATA STRUCTURES/Homework/Store With A Linked List/Node.java
+++ DATA STRUCTURES/Homework/Store With A Linked List/Node.java
@@ -1,1 +1,14 @@
+/* Class for a single node. A node consists of a value
+ * and a reference (or pointer) to the next node. In 
+ * the case of a single node the pointer points to null */
+
+public class Node {
+	public String value;
+	public Node next;
+	
+	public Node(String val) {
+		value = val;
+		next = null;
+	}
+}
 
