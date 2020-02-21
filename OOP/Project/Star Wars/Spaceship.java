907483bc89dd7bf0c739d303edbfad71f2ad136d
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-21 17:36:29 -0500

    Update Spaceship.java

--- OOP/Project/Star Wars/Spaceship.java
+++ OOP/Project/Star Wars/Spaceship.java
@@ -1,9 +1,1 @@
-public class Spaceship extends MobileImages{
-	/* This class inherits the MobileImages contructor that has 5 parameters from the MobileImages class.
-	 * This class also inherits all the other properties of the MobileImages class.
-	 * */
-	public Spaceship(int premier_X, int premier_Y, int dX, int delay, int test) {
-		super(premier_X, premier_Y, dX, delay, test);
-	}
-}
 
