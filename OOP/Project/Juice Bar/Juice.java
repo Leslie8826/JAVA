0eea56cc9326ffc5b52a225370943cecf5d9db02
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-23 13:06:08 -0500

    Update Juice.java

--- OOP/Project/Juice Bar/Juice.java
+++ OOP/Project/Juice Bar/Juice.java
@@ -1,1 +1,18 @@
+/**
+ * This class represents the blueprint of the Juice object 
+ * */
+
+public class Juice {
+	String juice;
+	
+	// Method to set the name/type of juice
+	void setName(String str) {
+		juice = str;
+	}
+	
+	// Method to get the name/type of juice
+	String getName(){
+		return juice;
+	}
+}
 
