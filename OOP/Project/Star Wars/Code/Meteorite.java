4312dc08f6dc2f3d0c275de945700718fd1cfd2f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-23 12:54:40 -0500

    Update Meteorite.java

--- OOP/Project/Star Wars/Code/Meteorite.java
+++ OOP/Project/Star Wars/Code/Meteorite.java
@@ -1,1 +1,10 @@
+public class Meteorite extends MobileImages{
+	/* This class inherits the MobileImages contructor that has 4 parameters from the MobileImages class.
+	 * This class also inherits all the other properties of the MobileImages class.
+	 * */
+	public Meteorite(int first_X, int first_Y, int dX, int delay) {
+		super(first_X, first_Y, dX, delay);
+	}
+}
+
 
