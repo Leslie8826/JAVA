f7192bb1f414bea8a410ddaac57c43db775897dd
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-02-23 13:02:29 -0500

    Update Bottle.java

--- OOP/Project/Juice Bar/Bottle.java
+++ OOP/Project/Juice Bar/Bottle.java
@@ -1,1 +1,51 @@
+/**
+ * This class represents the blueprint of the Bottle object 
+ * */
+
+public class Bottle {
+	Juice content = new Juice();
+	Juice[] contentList = new Juice[2];
+	int bottleCapacity=2000; // capacity in millimiters
+	int quantity=bottleCapacity; // we assume that each bottle is full
+	int checkQuantPoured;
+	
+	// Constructor with 1 parameter
+	Bottle(Juice j){
+		content = j;
+	}
+	
+	Bottle(Juice j1, Juice j2){
+		contentList[0] = j1;
+		contentList[1] = j2;
+	}
+	
+	// Method to pour juice from bottle
+	int pour(int q) {
+		quantity = quantity - q;
+		checkQuantPoured = q;
+		
+		if(quantity > 0) {
+			return quantity;
+		} else {
+			System.out.println("There is no " + content.getName() + " juice left");
+			return 0;
+		}
+	}
+	
+	// Method to check the quantity poured from the bottle
+	int checkQuantityPoured() {
+		return this.checkQuantPoured;
+		
+	}
+	
+	// Method to get the quantity left in bottle
+	int getQuantityBottle() {
+		return quantity;
+	}
+	
+	// Method to get the name/type of juice in the bottle
+	String getName() {
+		return content.getName();
+	}
+}
 
