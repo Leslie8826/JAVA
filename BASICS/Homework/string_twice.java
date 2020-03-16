726fc2f97450216cd5e8101eb9861a86a0c5e9e5
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-03-16 17:51:35 -0400

    Update string_twice.java

--- BASICS/Homework/string_twice.java
+++ BASICS/Homework/string_twice.java
@@ -1,1 +1,30 @@
+import java.util.Enumeration;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class string2 {
+
+	public static void main(String[] args) {
+		Stack<String> sta = new Stack<String>();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println(" Enter a String: ");
+		String str = input.nextLine();
+		
+    // Insert elements in stack
+		sta.push(str.toLowerCase());
+		sta.push(str.toUpperCase());
+		
+		Enumeration enu = sta.elements();
+		
+		// Display elements of stack
+		System.out.println("\n Display 1:");
+		while (enu.hasMoreElements()) {
+			System.out.print(enu.nextElement());
+    }
+			
+		}
+	}
+
+}
 
