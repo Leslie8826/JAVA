80219d2322243b34b56c79382c60f86fcb0af959
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-03-18 22:58:24 -0400

    Update MyStack.java

--- DATA STRUCTURES/Homework/Stack Implementation - Array/MyStack.java
+++ DATA STRUCTURES/Homework/Stack Implementation - Array/MyStack.java
@@ -1,1 +1,55 @@
+public class MyStack {
+	int max = 10;
+	int top = -1;
+	int index = 0;
+	int[] a = new int[max];
+	
+	// isEmpty method
+	boolean isEmpty() {
+		if(top < 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// push method
+	int push(int numb) {
+		a[index] = numb;
+		top = a[index];
+		index = index+1;
+		
+		return index;
+	}
+	
+	// display method
+	void display() {
+		if(index==0) {
+			System.out.print(a[0] + " ");
+		} else {
+			for(int i=0; i<index; i++) {
+				System.out.print(a[i] + " ");
+			}
+		}
+	}
+	
+	// Peek method
+	void peek() {
+		if(index < 0) {
+			System.out.println(" No element in the stack!!");
+		} else if(index > 0){
+			System.out.println(a[index-1]);
+		} else if(index == 0) {
+			System.out.println(a[0]);
+		}
+	}
+	
+	// Pop method
+	int pop() {
+		a[index-1] = 0;
+		index = index - 1;
+		return index;
+	}
+	
+}
 
