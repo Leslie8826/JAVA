d4571ce4b4949d5afe4b0495e7f190fbd8d9aaec
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-04-03 23:22:32 -0400

    Update test_queue.java

--- DATA STRUCTURES/Homework/Queue/test_queue.java
+++ DATA STRUCTURES/Homework/Queue/test_queue.java
@@ -1,1 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class test_queue {
+	public static void main(String[] args) {
+		Queue<Integer> list = new LinkedList<Integer>();
+		
+		System.out.println("Checking if queue is empty:");
+		System.out.println(list.isEmpty());
+		
+		System.out.println("\n Enqueue elements:");
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		list.add(6);
+		list.add(7);
+		list.add(8);
+		list.add(9);
+		
+		System.out.println("\n Checking if queue is empty:");
+		System.out.println(list.isEmpty());
+		
+		System.out.println("\n Display list: ");
+		System.out.print("Front " + list + " Rear");
+		
+		System.out.println("\n\n Dequeue elements:");
+		list.remove();
+		list.remove();
+		list.remove();
+		
+		System.out.println("\n Display list: ");
+		System.out.print("Front " + list + " Rear");
+		
+	}
+
+}
 
