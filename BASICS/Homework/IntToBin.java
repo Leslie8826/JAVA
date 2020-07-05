e800bf11ea29ddd97f97b988f46d9a543d826dbe
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-04 20:52:14 -0400

    Update IntToBin.java

--- BASICS/Homework/IntToBin.java
+++ BASICS/Homework/IntToBin.java
@@ -1,1 +1,33 @@
+// This program converts an integer to a binary number
 
+import java.util.Stack;
+import java.util.Scanner;
+
+public class IntToBin {
+	
+	public void IntToBinary(int d) {
+		Stack<Integer> s = new Stack<Integer>();
+		int res=d;
+		
+		while(res > 0) {
+			s.add(res%2);
+			res = res/2;
+		}
+		
+		System.out.print(d + " --> Integer to binary --> ");
+		
+		for(int i = s.size()-1; i >= 0 ; i--) {
+			System.out.print(s.get(i) + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+		int d; 
+		Scanner scan = new Scanner(System.in);
+		System.out.println(" Enter a number: ");
+		d = scan.nextInt();
+	
+		IntToBin test = new IntToBin();
+		test.IntToBinary(d);
+	}
+}
