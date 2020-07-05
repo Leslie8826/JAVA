1ec537c1ffba04979859da2f0e47c4875bd2a7ee
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-05 14:48:26 -0400

    Update LinearSearch.java

--- Review/Searching algorithms/Linear Search/LinearSearch.java
+++ Review/Searching algorithms/Linear Search/LinearSearch.java
@@ -1,1 +1,28 @@
+// This program searches an element in a
+// sorted array using a linear search algorithm
+
+public class LinearSearch {
+	int linearSearch(int[] a, int x) {
+		
+		for(int i=0; i<a.length; i++) {
+			if(a[i] == x) 
+				return i;
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] array = {2, 5, 12, 700, 709, 1200};
+		int nb = 1200;
+		
+		LinearSearch test = new LinearSearch();
+		
+	  int t = test.linearSearch(array, nb);
+	    
+	  if(t>=0)
+	    System.out.println(nb + " is at index " + t);
+	  else
+	    System.out.println(nb + " cannot be found");
+	}
+}
 
