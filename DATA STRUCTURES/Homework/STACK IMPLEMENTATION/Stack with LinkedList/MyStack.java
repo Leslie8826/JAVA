d01222884b416207c90f9306687c92d599b10664
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-07 21:54:28 -0400

    Update MyStack.java

--- DATA STRUCTURES/Homework/STACK IMPLEMENTATION/Stack with LinkedList/MyStack.java
+++ DATA STRUCTURES/Homework/STACK IMPLEMENTATION/Stack with LinkedList/MyStack.java
@@ -1,1 +1,44 @@
+public class MyStack {
+	MyNode top;
+	
+	MyStack(){
+		this.top = null;
+	}
+	
+	public void push(int val) {
+		MyNode node = new MyNode(val);
+		
+		node.next = top;
+		top = node;
+	}
+	
+	public boolean isEmpty() {
+		return top==null;
+	}
+	
+	public int peek() {
+		if(top == null) {
+			return -1;
+		} else {
+			return this.top.data;
+		}
+	}
+	
+	
+	public void pop() {
+		this.top = this.top.next;
+	}
+	
+	
+	public void display() {
+		MyNode node = this.top;
+		
+		while(node.next != null) {
+			System.out.print(node.data + " -> ");
+			node = node.next;
+		}
+		
+		System.out.println(node.data);
+	}
+}
 
