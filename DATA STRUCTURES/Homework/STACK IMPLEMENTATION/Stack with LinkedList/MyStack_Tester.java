dad0676d238a5b5da3188ac9eef68a655c7b25bd
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-07 21:54:03 -0400

    Update MyStack_Tester.java

--- DATA STRUCTURES/Homework/STACK IMPLEMENTATION/Stack with LinkedList/MyStack_Tester.java
+++ DATA STRUCTURES/Homework/STACK IMPLEMENTATION/Stack with LinkedList/MyStack_Tester.java
@@ -1,54 +1,39 @@
+
 public class MyStack_Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		MyStack stack = new MyStack();
+		System.out.println("Is Stack empty? --> " + stack.isEmpty());
 		
-		System.out.println("Add 5: ");
+		System.out.println("\n Add 4:");
+		stack.push(4);
+		stack.display();
+		System.out.println("\n Is Stack empty? --> " + stack.isEmpty());
+		
+		System.out.println("\n Add 5:");
 		stack.push(5);
 		stack.display();
 		
-		System.out.println("Add 10: ");
-		stack.push(10);
+		System.out.println("\n Add 0:");
+		stack.push(0);
 		stack.display();
 		
-		System.out.println("Add 16: ");
-		stack.push(16);
+		System.out.println("\n Add -6:");
+		stack.push(-6);
 		stack.display();
 		
-		System.out.println("\n Add 1: ");
-		stack.push(1);
-		stack.display();
-		
-		System.out.println("\n Add 1: ");
-		stack.push(3);
-		stack.display();
-		
-		System.out.println("\n Add 1: ");
-		stack.push(9);
-		stack.display();
-		
-		System.out.println("\n My stack: ");
-		stack.display();
-		
-		System.out.print("\n Peek = ");
-		stack.peek();
-		
-		System.out.println("");
-		System.out.println("Pop:");
-		stack.pop();
-		stack.display();
-		System.out.println("");
-		
-		System.out.println("Pop:");
-		stack.pop();
-		stack.display();
-		System.out.println("");
-		
-		System.out.print("Peek = ");
-		stack.peek();
-	
+	    System.out.println("Top element in stack =  " + stack.peek());
+	    
+	    System.out.println("\n Remove top element:");
+	    stack.pop();
+	    stack.display();
+	    
+	    System.out.println("\n Remove top element:");
+	    stack.pop();
+	    stack.display();
+	    
 	}
 
 }
