e6623b4a687baf64135fb24304358151dd60551a
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-13 14:43:15 -0400

    Create Sorting_an_Array_with_TreeSet.java

---
+++ Review/Sorting an array with a TreeSet/Sorting_an_Array_with_TreeSet.java
@@ -0,0 +1,44 @@
+/**
+* 
+* This program sorts an array of distinct integers using a TreeSet
+* 
+* */
+
+import java.util.*;
+
+public class Sorting_an_Array_with_TreeSet {
+	public static void main(String args[]) {
+		int[] a = {77, 23, 4, 66, 99, 112, 45, 56, 39, 89};
+		TreeSet<Integer> a_sorted = new TreeSet<Integer>();
+	    
+		System.out.println("Elements of the array: ");
+	    for(int i=0; i<a.length; i++) {
+	    	System.out.print(a[i] + "  ");
+	    }
+	    
+	    // Adds elements of array into treeset
+	    for(int i=0; i<a.length; i++) {
+	    	a_sorted.add(a[i]);
+	    }
+	    
+	    // Prints elements of treeset
+	    System.out.println("\n");
+	    System.out.println("Elements of the array - sorted with TreeSet: ");
+	    System.out.println(a_sorted);
+	    
+	}
+	
+}
+
+/************************* OUTPUT *************************************/
+/**
+*
+*
+* Elements of the array: 
+* 77  23  4  66  99  112  45  56  39  89  
+* 
+* Elements of the array - sorted with TreeSet: 
+* [4, 23, 39, 45, 56, 66, 77, 89, 99, 112]
+*
+*
+**/
