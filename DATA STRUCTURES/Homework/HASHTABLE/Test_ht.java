7e9f974be540d86fa4dc26f918ea5d42cf604f9f
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-13 14:27:02 -0400

    Update Test_ht.java

--- DATA STRUCTURES/Homework/HASHTABLE/Test_ht.java
+++ DATA STRUCTURES/Homework/HASHTABLE/Test_ht.java
@@ -1,1 +1,52 @@
 
+/**
+ * 
+ * This program list distinct characters and counts the number of times 
+ * each appears in a given string
+ * 
+ * **/
+
+//import java.util.Hashtable;
+//import java.util.Enumeration;
+import java.util.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Hellooo";
+		Hashtable<Character, Integer> count = new Hashtable<Character, Integer>();
+		Set<Character> keys = count.keySet();
+		
+		// Adds the characters from string into the hashtable
+		for(int i=0; i<str.length(); i++) {
+			count.put(str.charAt(i), 0);
+		}
+		
+    // Prints incomplete hashtable
+		System.out.print(count);
+		
+    // insert values of corresponding each key into hashtable
+		for(Character key : keys) {
+			int countKeys=0;
+			for(int i=0; i<str.length(); i++) {
+				if(key == str.charAt(i)) {
+					countKeys++;
+					count.put(key, countKeys);
+				}
+			}
+		}
+		
+		
+		// Prints hashtable with enumeration interface
+		Enumeration<Character> enumeration = count.keys();
+		
+		System.out.println("\n");
+		while(enumeration.hasMoreElements()) {
+			Character key2 = enumeration.nextElement();
+			System.out.println("character: "+ key2 + ", occurence = " + count.get(key2));
+		}
+
+	}
+
+}
