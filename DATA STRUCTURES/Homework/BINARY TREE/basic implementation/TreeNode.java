07437fca38d16edaa9d4dbf954b4fe7d5ae3c01a
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-21 22:34:24 -0400

    Create TreeNode.java

---
+++ DATA STRUCTURES/Homework/BINARY TREE/basic implementation/TreeNode.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * This class represents the node of the binary tree
+ *
+**/
+
+public class TreeNode {
+	int data;
+	TreeNode left;
+	TreeNode right;
+	
+	TreeNode(int data){
+		this.data = data;
+		this.left = this.right = null;
+	}
+}
