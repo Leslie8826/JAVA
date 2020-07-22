8448f26fa060cf42c3057b6939f5e7613a0b65fa
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-21 22:33:26 -0400

    Create TestBinTree.java

---
+++ DATA STRUCTURES/Homework/BINARY TREE/basic implementation/TestBinTree.java
@@ -0,0 +1,42 @@
+public class TestBinTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryTree tree = new BinaryTree();
+		
+		tree.root = new TreeNode(9);
+		tree.root.left = new TreeNode(11);
+		tree.root.right = new TreeNode(14);
+		
+		tree.root.left.left = new TreeNode(5);
+		tree.root.left.right = new TreeNode(1);
+		
+		tree.root.left.left.left = new TreeNode(50);
+		tree.root.left.left.right = new TreeNode(100);
+		
+		System.out.println("In Order Traversal:");
+		tree.PrintInOrder(tree.root);
+		
+		System.out.println("\n");
+		System.out.println("Post Order Traversal:");
+		tree.PrintPostOrder(tree.root);
+		
+		System.out.println("\n");
+		System.out.println("Pre Order Traversal:");
+		tree.PrintPreOrder(tree.root);
+		
+		System.out.println("\n");
+		System.out.println("Is " + tree.root.data + " a leaf ?");
+		System.out.println(tree.isLeaf(tree.root));
+		
+		System.out.println("\n");
+		System.out.println("Is " + tree.root.left.right.data + " a leaf ?");
+		System.out.println(tree.isLeaf(tree.root.left.right));
+		
+		System.out.println("\n");
+		System.out.println("Height of " + tree.root.data + " = ");
+		System.out.println(tree.getHeight(tree.root));
+	
+	}
+
+}
