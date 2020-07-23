92eb26e4b1c6a02ba833f2546a38879d09bc180c
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-23 19:28:01 -0400

    Update TestGrapgh.java

--- DATA STRUCTURES/Homework/GRAPH/Graph implementation with adjacency matrix/TestGrapgh.java
+++ DATA STRUCTURES/Homework/GRAPH/Graph implementation with adjacency matrix/TestGrapgh.java
@@ -1,1 +1,21 @@
 
+public class Test {
+	public static void main(String[] args) {
+    Graph graph = new Graph(5);
+    
+		graph.addEdge(0, 1); // adds edge between vertex 0 and vertex 1
+    graph.addEdge(0, 4); // adds edge between vertex 0 and vertex 4
+    graph.addEdge(1, 2); // adds edge between vertex 1 and vertex 2
+    graph.addEdge(1, 3); // adds edge between vertex 1 and vertex 3
+    graph.addEdge(1, 4); // adds edge between vertex 1 and vertex 4
+    graph.addEdge(2, 3); // adds edge between vertex 2 and vertex 3
+    graph.addEdge(3, 4); // adds edge between vertex 3 and vertex 4
+        
+    // Displays graph vertices
+    graph.printGraph();
+        
+    // Displays vertices and their connections
+    graph.VertexConnection();
+  }
+}
+
