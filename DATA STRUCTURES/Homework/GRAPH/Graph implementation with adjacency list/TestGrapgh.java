70d5738bb174ec70b2daee0deb727e6120e02544
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-23 18:32:25 -0400

    Update TestGrapgh.java

--- DATA STRUCTURES/Homework/GRAPH/Graph implementation with adjacency list/TestGrapgh.java
+++ DATA STRUCTURES/Homework/GRAPH/Graph implementation with adjacency list/TestGrapgh.java
@@ -2,15 +2,15 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// Graph with 4 vertices
 		Graph graph = new Graph(4);
 		
-		graph.addEdge(0, 1);
-		graph.addEdge(0, 3);
-		graph.addEdge(1, 2);
-		graph.addEdge(1, 3);
-		graph.addEdge(2, 3);
+		// Adds the edges between the vertices
+		graph.addEdge(0, 1); // edge between vertex 0 and vertex 1
+		graph.addEdge(0, 3); // edge between vertex 0 and vertex 3
+		graph.addEdge(1, 2); // edge between vertex 1 and vertex 2
+		graph.addEdge(1, 3); // edge between vertex 1 and vertex 3
+		graph.addEdge(2, 3); // edge between vertex 2 and vertex 3
 		
 		System.out.println("Graph - PRINT 1:");
 		graph.printGraph();
@@ -19,7 +19,9 @@ public class Test {
 		System.out.println("Graph - PRINT 2:");
 		graph.print_Graph();
 		
+		
 		System.out.println(" ");
+		// Prints the connections of each vertex
 		graph.VertexConnections();
 
 	}
