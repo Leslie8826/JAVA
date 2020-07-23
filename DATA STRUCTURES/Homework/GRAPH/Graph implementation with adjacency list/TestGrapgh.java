298fb6bb7fba5c1e289bfe58a4102e0eb6f4aa79
Leslie8826 <48226946+Leslie8826@users.noreply.github.com>
2020-07-23 18:28:11 -0400

    Update TestGrapgh.java

--- DATA STRUCTURES/Homework/GRAPH/Graph implementation with adjacency list/TestGrapgh.java
+++ DATA STRUCTURES/Homework/GRAPH/Graph implementation with adjacency list/TestGrapgh.java
@@ -1,1 +1,28 @@
 
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Graph graph = new Graph(4);
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(1, 2);
+		graph.addEdge(1, 3);
+		graph.addEdge(2, 3);
+		
+		System.out.println("Graph - PRINT 1:");
+		graph.printGraph();
+		
+		System.out.print("\n");
+		System.out.println("Graph - PRINT 2:");
+		graph.print_Graph();
+		
+		System.out.println(" ");
+		graph.VertexConnections();
+
+	}
+
+}
+
